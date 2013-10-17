da61058afef7801e505ac0acb2f1a05feab752ac
Yong Bakos <ybakos@humanoriented.com>
2013-10-17 11:42:32 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+CSE 5343 Project 5
+==================
+This repository contains my work for Project 5, building a simple Unix shell in C.
---
+++ shell.c
@@ -0,0 +1,11 @@
+/* shell.c
+   A simple REPL / shell for Unix, for CSE 5343 Project 5
+   Yong Bakos
+*/
+
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+  printf("Hello World!\n");
+  return 0;
+}
